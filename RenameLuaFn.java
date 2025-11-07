85aaf5e6213af1d6682ff6bff4762fed4881f094
bgkillas <walhelm@proton.me>
2025-11-07 11:20:34 -0500

    parse incomming data types

--- RenameLuaFn.java
+++ RenameLuaFn.java
@@ -1,19 +1,29 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.Stream;
 import ghidra.app.cmd.function.ApplyFunctionSignatureCmd;
 import ghidra.app.decompiler.flatapi.FlatDecompilerAPI;
 import ghidra.app.script.GhidraScript;
 import ghidra.app.script.GhidraState;
+import ghidra.app.services.DataTypeManagerService;
 import ghidra.program.flatapi.FlatProgramAPI;
 import ghidra.program.model.address.Address;
 import ghidra.program.model.address.AddressFactory;
 import ghidra.program.model.address.AddressSpace;
+import ghidra.program.model.data.ArrayDataType;
+import ghidra.program.model.data.BuiltInDataTypeManager;
 import ghidra.program.model.data.CategoryPath;
 import ghidra.program.model.data.DataType;
 import ghidra.program.model.data.DataTypeConflictHandler;
+import ghidra.program.model.data.DataTypeManager;
+import ghidra.program.model.data.EnumDataType;
 import ghidra.program.model.data.FunctionDefinitionDataType;
 import ghidra.program.model.data.ParameterDefinition;
 import ghidra.program.model.data.PointerDataType;
 import ghidra.program.model.data.ProgramBasedDataTypeManager;
 import ghidra.program.model.data.StructureDataType;
+import ghidra.program.model.data.UnionDataType;
 import ghidra.program.model.listing.Function;
 import ghidra.program.model.listing.FunctionSignature;
 import ghidra.program.model.listing.Program;
@@ -29,6 +39,8 @@ public class RenameLuaFn extends GhidraScript {
     AddressSpace space;
     ProgramBasedDataTypeManager dtm;
     SourceType source = SourceType.USER_DEFINED;
+	Map<String, String> map = new HashMap<>();
+    DataTypeManagerService svc;
     protected void run() throws Exception {
         gstate = this.getState();
         program = gstate.getCurrentProgram();
@@ -37,8 +49,142 @@ public class RenameLuaFn extends GhidraScript {
         addressFactory = program.getAddressFactory();
         space = addressFactory.getDefaultAddressSpace();
         dtm = program.getDataTypeManager();
+        svc = state.getTool().getService(DataTypeManagerService.class);
+    	map.put("usize", "uint");
+    	map.put("isize", "int");
+    	map.put("f32", "float");
+    	map.put("f64", "double");
+    	for (int i = 8; i < 128; i *= 2) {
+    		map.put("u"+i, "uint"+i+"_t");
+       		map.put("i"+i, "int"+i+"_t");
+    	}
+    	System.out.println(map);
+        parse_rust();
     	rename_lua_fn();
-    	String[] names = {"entity_manager_ptr", "world_seed", "new_game_count",
+    	rename_globals();
+    	rename_functions();
+    }
+    
+    void parse_rust() throws Exception {
+    	String out = "struct T a:*[**u64;3] b:isize c:bool d:char e:u64"
+    			+ "\nenum K A:-1 B:0"
+    			+ "\nunion Y a:*isize b:usize";
+    	Stream<String> lines = out.lines();
+    	for (String line : lines.toList()) {
+    		String[] split = line.split(" ");
+    		String name = split[1];
+    		if (name.endsWith(">")) {
+    			continue;
+    		}
+    		if (split[0].equals("struct")) {
+    			StructureDataType struct = create_struct(name);
+    			for (int i = 2; i < split.length; i++) {
+    				String[] pair = split[i].split(":");
+    				String component = pair[0];
+    				String value = pair[1];
+    				DataType type = parse_type(value);
+    				struct.add(type, type.getLength(), component, "");
+    			}
+    			dtm.addDataType(struct, DataTypeConflictHandler.REPLACE_HANDLER);
+    		} else if (split[0].equals("union")) {
+    			UnionDataType union = create_union(name);
+    			for (int i = 2; i < split.length; i++) {
+    				String[] pair = split[i].split(":");
+    				String component = pair[0];
+    				String value = pair[1];
+    				DataType type = parse_type(value);
+    				union.add(type, type.getLength(), component, "");
+    			}
+    			dtm.addDataType(union, DataTypeConflictHandler.REPLACE_HANDLER);    			
+    		} else if (split[0].equals("enum")) {
+    			EnumDataType enumt = create_enum(name);
+    			for (int i = 2; i < split.length; i++) {
+    				String[] pair = split[i].split(":");
+    				String component = pair[0];
+    				String value = pair[1];
+    				enumt.add(component, Long.parseLong(value), "");
+    			}
+    			dtm.addDataType(enumt, DataTypeConflictHandler.REPLACE_HANDLER);    			    			
+    		}
+    	}	
+    }
+    
+    DataType parse_type(String name) {
+		if (name.startsWith("*")) {
+			name = name.substring(1);
+			System.out.println(name);
+			return new PointerDataType(parse_type(name));
+		}
+		if (name.startsWith("[") && name.endsWith("]")) {
+			name = name.substring(1, name.length() - 1);
+			int split = name.lastIndexOf(";");
+			int len = Integer.parseInt(name.substring(split+1));
+			name = name.substring(0, split);
+			System.out.println(name);
+			return new ArrayDataType(parse_type(name), len);			
+		}
+		if (map.containsKey(name)) {
+			System.out.println(name);
+			name = map.get(name);
+			System.out.println(name);
+		}
+		return get_type(name);
+    }
+
+    DataType get_type(String name) {
+        for (DataTypeManager datatypemanager : svc.getDataTypeManagers()) {
+        	DataType type = find_type_in_manager(datatypemanager, name);
+        	if (type != null) {
+        		return type;
+        	}
+        }
+        return null;
+    }
+    
+    private DataType find_type_in_manager(DataTypeManager datatypemanager, String name) {
+    	System.out.println();
+        Iterator<DataType> types = datatypemanager.getAllDataTypes();
+        while (types.hasNext()) {
+            DataType type = types.next();
+            if (type.getName().startsWith("uint")) {
+            	System.out.println(name);
+            	System.out.println(type.getName());
+            	System.out.println(name.length());
+            	System.out.println(type.getName().length());
+            }
+            if (type.getName().equals(name)) {
+                return type;
+            }
+        }
+        return null;
+    }
+    StructureDataType create_struct(String name) {
+        CategoryPath category = new CategoryPath("/custom");
+        return new StructureDataType(category, name, 0);
+    }
+    
+    EnumDataType create_enum(String name) {
+        CategoryPath category = new CategoryPath("/custom");
+        return new EnumDataType(category, name, 1);
+    }
+    
+    UnionDataType create_union(String name) {
+        CategoryPath category = new CategoryPath("/custom");
+        return new UnionDataType(category, name);
+    }
+    
+    void rename_functions() throws Exception {
+    	String[] fn_names = {"get_entity", "kill_entity", "create_entity"};
+    	long[] fn_addrs = {0x0056eba0, 0x0044df60, 0x0056e590};
+    	for (int i = 0; i < fn_addrs.length; i++) {
+    		Address addr = space.getAddress(fn_addrs[i]);
+    		Function fn = fpapi.getFunctionAt(addr);
+    		fn.setName(fn_names[i], source);
+    	}
+    }
+
+    void rename_globals() throws Exception {
+       	String[] names = {"entity_manager_ptr", "world_seed", "new_game_count",
     			"global_stats", "game_global_ptr", "entity_tag_manager_ptr",
     			"component_type_manager", "component_tag_manager_ptr", "translation_manager",
     			"platform", "internal_filenames", "inventory_system",
@@ -53,15 +199,8 @@ public class RenameLuaFn extends GhidraScript {
     		Symbol sym = fpapi.getSymbolAt(addr);
     		sym.setName(names[i], source);
     	}
-    	String[] fn_names = {"get_entity", "kill_entity", "create_entity"};
-    	long[] fn_addrs = {0x0056eba0, 0x0044df60, 0x0056e590};
-    	for (int i = 0; i < fn_addrs.length; i++) {
-    		Address addr = space.getAddress(fn_addrs[i]);
-    		Function fn = fpapi.getFunctionAt(addr);
-    		fn.setName(fn_names[i], source);
-    	}
     }
-
+    
     DataType create_type(String name, int size) {
         DataType existing = dtm.getDataType("custom/" + name);
         if (existing != null) {
